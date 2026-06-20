19dfefb2d20e3943ba9b06b7064bffd82db438e6
trueprodprog <trueproductm@gmail.com>
2026-06-20 18:53:04 +0300

    Lesson 1 with i/o and Lesson 2 basic variables

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
-
+using namespace std; //now we don't need to write stdl
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //1st lesson
+    std::cout << "Hello, World! \n"; //this will output to the console hello world
+    //system("pause>0"); //only for windows
+
+    std::cout<< "My name is Mr" <<std::endl;
+    std::cout<< "Today is Saturday"<<endl;
+
+    cout<<"hello again"<<endl;
+    cout<<"Have a nice day" << endl;
+    //2nd lesson: variables
+    //float annualSalary = 50000.99;
+    float annualSalary;
+    cout<<"Enter your annual salary" << endl;
+    cin>>annualSalary;
+    float monthlySalary = annualSalary / 12;
+    cout<<"your monthly salary is " << monthlySalary << endl;
+    cout<<"In 10 years you will earn "<< annualSalary * 10;
+
+    char character = 'a'; //single quotation for variable
+    float firstSecondThird; //camelCase
+
     return 0;
 }
